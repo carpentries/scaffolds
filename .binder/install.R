cffe7a3e338772526c369b677e5beb9d6034a664
Zhian N. Kamvar <zkamvar@gmail.com>
2020-04-17 12:13:29 -0700

    update to use description

--- .binder/install.R
+++
@@ -1,9 +0,0 @@
-install.packages("glue")
-install.packages("dplyr")
-install.packages("ggplot2")
-install.packages("raster")
-install.packages("rgdal")
-install.packages("rasterVis")
-install.packages("remotes")
-install.packages("sf")
-
