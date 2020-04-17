af735b7abbcc516dbfd459d85d6f181c65a63f43
Zhian N. Kamvar <zkamvar@gmail.com>
2020-04-17 10:33:52 -0700

    add requirements and data download

--- .binder/install.R
+++ .binder/install.R
@@ -1,1 +1,8 @@
 install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("raster")
+install.packages("rgdal")
+install.packages("rasterVis")
+install.packages("remotes")
+install.packages("sf")
+
